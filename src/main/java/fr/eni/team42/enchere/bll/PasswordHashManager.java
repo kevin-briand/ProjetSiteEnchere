8adb247a52eae5b2462a4b1a2eb13e42ad65257e
Aurore Dubray <a.dubray64@gmail.com>
2022-12-06 11:39:17 +0100

    Correction char 256

--- src/main/java/fr/eni/team42/enchere/bll/PasswordHashManager.java
+++ src/main/java/fr/eni/team42/enchere/bll/PasswordHashManager.java
@@ -5,7 +5,7 @@ import java.security.NoSuchAlgorithmException;
 
 public class PasswordHashManager {
     static String hashMdp(String input) throws NoSuchAlgorithmException {
-        MessageDigest mDigest = MessageDigest.getInstance("SHA1");
+        MessageDigest mDigest = MessageDigest.getInstance("SHA256");
         byte[] result = mDigest.digest(input.getBytes());
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < result.length; i++) {
